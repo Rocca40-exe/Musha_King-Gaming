4af742baf2fdb750fbd4b02a3430c88f50d06e8f
Rocca40-exe <chrisrocc71@gmail.com>
2026-03-16 11:37:41 -0500

    Create App.tsx with main React component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Musha King Gaming!</h1>
+            <p>Get ready to dive into an exciting gaming experience.</p>
+        </div>
+    );
+};
+
+export default App;
